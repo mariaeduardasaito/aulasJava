3e7c4542b8e6a6e5f8f308602dd70316520b7925
Laboratorio <laboratorio@escola.local>
2026-08-12 11:18:18 -0300

    Criacao Classes

---
+++ Aluno.java
@@ -0,0 +1,18 @@
+class Aluno extends Pessoa{
+    private double notaFinal;
+
+    public Aluno(double notaFinal, String nome, int idade) {
+        super(nome, idade);
+        this.notaFinal = notaFinal;
+    }
+    public double getNotaFinal() {
+        return notaFinal;
+    }
+    public void setNotaFinal(double notaFinal) {
+        this.notaFinal = notaFinal;
+    }
+    @Override
+    public String toString() {
+        return "Aluno{" + "notaFinal=" + notaFinal + ", nome=" + getNome() + ", idade=" + getIdade() + '}';
+    }
+}
---
+++ GerarPessoas.java
@@ -0,0 +1,14 @@
+public class GerarPessoas {
+    public static void main(String[] args) {
+
+        Pessoa pessoaUm = new Pessoa(62);
+         System.out.println(pessoaUm);
+
+        Aluno alunoUm = new Aluno(10, "Mario", 21);
+        System.out.println(alunoUm);
+        alunoUm.setIdade(23);
+        System.out.println("Nova idade do aluno: " + alunoUm.getIdade());
+
+       
+    }
+}
